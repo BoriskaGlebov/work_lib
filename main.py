417aed648c7883a91161382207abfc53ab5693f2
Boriska <boristheblade.glebov@yandex.ru>
2024-02-15 06:59:41 +0300

    File

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print('[fq')
+print(r' pip freeze >requirements.txt    ')
+print(r'pip download -r .\requirements.txt      ')
+print(r' pip install --no-index --find-links pkg -r .\requirements.txt')
