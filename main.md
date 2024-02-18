89afe0a4befc6e7a885aa7a4274d78204d41a2d5
Boriska <boristheblade.glebov@yandex.ru>
2024-02-18 15:03:28 +0300

    Кое что

---
+++ main.md
@@ -0,0 +1,5 @@
+### pip freeze >requirements.txt
+
+### pip download -r .\requirements.txt
+
+### pip install --no-index --find-links pkg -r .\requirements.txt
--- main.py
+++
@@ -1,3 +0,0 @@
-print(r' pip freeze >requirements.txt    ')
-print(r'pip download -r .\requirements.txt      ')
-print(r' pip install --no-index --find-links pkg -r .\requirements.txt')
