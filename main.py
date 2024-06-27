18ab7a49d9fc3c55b4e6fcb02201675824d2de80
BorisGlebov <boristhblade.glebov@yandex.ru>
2024-06-27 23:29:14 +0300

    дополнение

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import Levenshtein
 
 
 def replace(replace_str: str):
+    """
+    Функция заменяет возможные опечатки на моменте сравнения,
+    строка после проверки останется без изменений
+    :param replace_str: строка которую нужно сравнить
+    :return: измененная строка
+    """
     replace_str = replace_str.replace('O', '0')
     replace_str = replace_str.replace('G', '6')
     return replace_str
@@ -13,48 +19,45 @@ letters = string.ascii_uppercase + string.digits
 # print(letters)
 serial_numbers = []
 # генерирую диски db
-# with open('from_db.txt', 'w') as file:
-#     while len(serial_numbers) < 20:
-#         some_num = ''
-#         for _ in range(10):
-#             some_num += random.choice(letters)
-#         serial_numbers.append(some_num)
-#         file.write(some_num + '\n')
-# print(serial_numbers)
+with open('from_db.txt', 'w') as file:
+    while len(serial_numbers) < 400:
+        some_num = ''
+        for _ in range(10):
+            some_num += random.choice(letters)
+        serial_numbers.append(some_num)
+        file.write(some_num + '\n')
 # генерирую ошибочные в exel
-# with open('from_exel.txt', 'w') as file:
-#     for serial_num in serial_numbers:
-#         new_str = serial_num
-#         if random.randint(1, 3) == 1:
-#             new_str = serial_num.replace('O', '0')
-#             new_str = new_str.replace('G', '6')
-#         file.write(new_str + '\n')
-# смотрю сколько в итоге не совпадает
+# рандомно делает опечатки 'O' и '0', 'G' и '6'
+# рандомно заменяет один рандомный символ на любой другой, создает эффект отличия серийника на один символ
+with open('from_exel.txt', 'w') as file:
+    for serial_num in serial_numbers:
+        new_str = serial_num
+        if random.randint(1, 3) == 1:
+            new_str = serial_num.replace('O', '0')
+            new_str = new_str.replace('G', '6')
+        if random.randint(1, 2) == 1:
+            new_str = new_str.replace(serial_num[random.randint(0, len(new_str) - 1)], random.choice(letters))
+        file.write(new_str + '\n')
+
+# вот после подготовительных мероприятий приступаю собственно к сравнению
 with open('from_db.txt', 'r') as file1, open('from_exel.txt', 'r') as file2:
     from_db = file1.readlines()
     from_exel = file2.readlines()
+    # в этот список кладу все отличающиеся серийники из БД
     out = []
-    for num1, num2 in zip(from_db, from_exel):
-        if num1 != num2:
-            # print(num1.strip(), num2)
-            out.append((num1, num2))
-        # else:
-        # print(num1.strip(), num2)
-    # print(len(out))
-
-    for from_db, from_exel in out:
-        if Levenshtein.distance(from_db,from_exel,processor=replace):
-            print(from_db,from_exel)
+    for hdd in from_db:
+        for hdd_exel in from_exel:
+            if hdd == hdd_exel or Levenshtein.distance(hdd, hdd_exel, processor=replace) == 0:
+                # Сюда можно дописать, что делать если диски одинаковые,
+                # например поместить в какой-то словарь, где будут данные из БД и EXEL
+                # для вставки в таблицу на сайте.
+                # Так же если данные из exel это тоже список,
+                # то я бы удалял из него все что находим,
+                # тем самым думаю немного будет ускоряться поиск
+                break
+            else:
+                continue
         else:
-            print(from_db,from_exel)
-        # print(Levenshtein.distance(from_db, from_exel, processor=replace))
-# a = 'BNMOBYN34GX'
-# c = 'BNM0BYN34X6'
-# d = 'BNMOBYN34M'
-# print(Levenshtein.distance(a, c, processor=replace))
-# # print(Levenshtein.distance(a, d, weights=(1, 1, 2)))
-# #
-# # print(Levenshtein.jaro_winkler(a, d, prefix_weight=0.25))
-# print((Levenshtein.ratio(a, c,processor=replace)))
-# # print((Levenshtein.ratio(a,d)))
-# print(replace(a))
+            out.append(hdd)
+    print(len(out))
+    print(out)
